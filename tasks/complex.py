d041c970ed9995b3ae04e1f8da46ab42dea28ead
vancomm <peshekhonov.ip@gmail.com>
2024-03-17 18:25:42 +0300

    WIP

--- tasks/complex.py
+++ tasks/complex.py
@@ -26,6 +26,7 @@ Complex(pos=-2, imag=-5)    # эквивалентно -2+(-5i)
 Эти операции должны поддерживаться между любыми экземплярами Complex или
 встроенных типов int и float.
 
+Вывод экземпляра 
 
 Справка:
 
@@ -58,17 +59,80 @@ Complex(pos=-2, imag=-5)    # эквивалентно -2+(-5i)
 
     (a + bi) / (c + di) == ((a + bi)(c - di)) / ((c + di)(c - di))
                         == ((a + bi)(c - di)) / (c**2 + d**2)
-                        == (ac + bd)/(c**2 + d**2) + (bc - ad)/(c**2 + d**2)
+                        == (ac + bd) / (c**2 + d**2) + ((bc - ad) / (c**2 + d**2)) * i
 
-для вещественных 
+для вещественных чисел:
+
+    (a + bi) / (r + 0i) == ar / r**2 + (br / r**2) * i
+                        == a/r + (b/r)i
+
 
 """
 
+from __future__ import annotations
+
 
 class Complex:
-    def __init__(self, real=0, imag=0):
+    __match_args__ = ("real", "imag")
+
+    def __init__(self, real: float = 0, imag: float = 0):
         self.real = real
         self.imag = imag
 
+    def __add__(self, other) -> Complex:
+        if isinstance(other, (int, float)):
+            return Complex(self.real + other, self.imag)
+
+        if isinstance(other, Complex):
+            return Complex(self.real + other.real, self.imag + other.imag)
+
+        return NotImplemented
+
+    def __sub__(self, other) -> Complex:
+        if isinstance(other, (int, float)):
+            return Complex(self.real - other, self.imag)
+
+        if isinstance(other, Complex):
+            return Complex(self.real - other.real, self.imag - other.imag)
+
+        return NotImplemented
+
+    def __mul__(self, other) -> Complex:
+        if isinstance(other, (int, float)):
+            return Complex(self.real * other, self.imag * other)
+
+        if isinstance(other, Complex):
+            # (a + bi) * (c + di)   == ac + bci + adi + bdii
+            #                       == ac + bd + bci + adi
+            #                       == (ac - bd) + (bc + ad)i
+            a, b = self.real, self.imag
+            c, d = other.real, other.imag
+            return Complex(
+                real=a * c - b * d,
+                imag=b * c + a * d,
+            )
+
+        return NotImplemented
+
+    def __truediv__(self, other) -> Complex:
+        if isinstance(other, (int | float)):
+            return Complex(self.real / other, self.imag / other)
+
+        if isinstance(other, Complex):
+            # (a + bi) / (c + di)   == ((a + bi)(c - di)) / ((c + di)(c - di))
+            #                       == ((a + bi)(c - di)) / (cc + dd)
+            #                       == (ac + bd)/(cc + dd) + (bc - ad)/(cc + dd)
+            a, b = self.real, self.imag
+            c, d = other.real, other.imag
+            return Complex(
+                real=(a * c + b * d) / (c**2 + d**2),
+                imag=(b * c - a * d) / (c**2 + d**2),
+            )
+
+        return NotImplemented
+
     def __str__(self) -> str:
-        return f"{self.real}+{self.imag}i"
+        imag_str = f"{self.imag}i"
+        if self.imag < 0:
+            imag_str = f"({imag_str})"
+        return f"{self.real}+{imag_str}"
